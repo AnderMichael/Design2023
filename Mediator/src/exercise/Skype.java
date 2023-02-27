183e386bcd716cbb225481d6f8cc6381f5658671
AnderMichael <andercaymi@gmail.com>
2023-02-27 17:17:54 -0400

    Mediator con Map

--- Mediator/src/exercise/Skype.java
+++ Mediator/src/exercise/Skype.java
@@ -1,42 +1,30 @@
 package exercise;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Skype implements ICanalComunicacion {
 	private List<TeamMember> globalTeam = new ArrayList<>();
-	private List<QA> teamQA = new ArrayList<>();
-	private List<SM> teamSM = new ArrayList<>();
-	private List<DEV> teamDEV = new ArrayList<>();
+	private List<TeamMember> teamQA = new ArrayList<>();
+	private List<TeamMember> teamSM = new ArrayList<>();
+	private List<TeamMember> teamDEV = new ArrayList<>();
+
+	private HashMap<String, List<TeamMember>> map = new HashMap<>();
+
+	public Skype() {
+		map.put("QA", teamQA);
+		map.put("DEV", teamDEV);
+		map.put("SM", globalTeam);
+	}
 
 	@Override
 	public void sendMessage(String message, TeamMember member) {
-		switch (member.getCargo()) {
-		case "QA": {
-			for (QA qa : teamQA) {
-				if (!qa.equals(member)) {
-					qa.reciveMessage(message);
-				}
+		List<TeamMember> groupMembers = map.get(member.getCargo());
+		for (TeamMember teamMember : groupMembers) {
+			if (!teamMember.equals(member)) {
+				teamMember.reciveMessage(message);
 			}
-			break;
-		}
-		case "SM": {
-			for (TeamMember teamMember : globalTeam) {
-				if (!teamMember.equals(member)) {
-					teamMember.reciveMessage(message);
-				}
-			}
-		}
-		case "DEV": {
-			for (DEV dev : teamDEV) {
-				if (!dev.equals(member)) {
-					dev.reciveMessage(message);
-				}
-			}
-			break;
-		}
-		default:
-			break;
 		}
 
 	}
